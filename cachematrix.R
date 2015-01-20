3a75ea97ee55a412e57455f45e743a6cb7edac6f
c0nanh <mail@conanhales.com>
2015-01-20 16:39:43 +0000

    First commit
    Added logic to cache the inverse of the matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment 2 for rprog-010 coursera course
+## Learning to use the <<- operator and lexical scoping
+## <<- assigns a value different to the current environment
 
-## Write a short comment describing this function
+## Creates a list of matrices from the input matrix
+## List of 4 vectors:   1. set the value of the matrix
+##                      2. get the value of the matrix
+##                      3. set the value of the inverse
+##                      4. get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Calculates the inverse of the matrix unless a cache of the inverse exists
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of 'x'        
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
